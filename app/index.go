1db4dfc9c39eee147c22e29a86e512439108c2fe
Avoaja Joshua Akachi <avoajajoshua@gmail.com>
2023-06-29 18:19:15 +0100

    feat: configure cors support

--- app/index.go
+++ app/index.go
@@ -5,11 +5,13 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/joshDamian/web3storage-service/app/providers"
+	"github.com/gin-contrib/cors"
 )
 
 func App() *gin.Engine {
 	providers.Uploader.APIKey = os.Getenv("MORALIS_API_KEY")
 	router := gin.Default()
+	router.Use(cors.Default())
 	router.POST("/upload-file", providers.UploadSingleFile)
 	router.POST("/upload-files", providers.UploadMultipleFiles)
 	return router
